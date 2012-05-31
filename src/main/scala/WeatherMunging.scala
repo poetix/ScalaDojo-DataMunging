32390070d86df3d0bae16dc699c37ef88afc58c6
Dominic Fox <dominic.fox@gmail.com>
2012-05-31 16:56:18 +0100

    Use case classes for greater justice

--- src/main/scala/WeatherMunging.scala
+++ src/main/scala/WeatherMunging.scala
@@ -1,6 +1,22 @@
 package org.timgroup.dojo
 
-class Spread[T](val index:T,  val spread:Int)  { }
+abstract class Spread[T] {
+
+  val index:T
+  val spread:Int
+
+}
+
+case class Weather(data:Array[String]) extends Spread[Int] {
+  val intData = data.take(3).map(_.toInt)
+  val index = intData(0)
+  val spread = intData(1) - intData(2)
+}
+
+case class Football(data:Array[String]) extends Spread[String] {
+  val index = data(1)
+  val spread = (data(6).toInt - data(8).toInt).abs
+}
 
 case class MungerFor[T](val lines: List[String], interpreter:Array[String] => Spread[T]) {
   def munge =
@@ -12,19 +28,12 @@ case class MungerFor[T](val lines: List[String], interpreter:Array[String] => Sp
            .index
 }
 
-object Interpreters {
-  val toInts = (a:Array[String]) => a.take(3).map(_.toInt)
-  val _weather = (a:Array[Int]) => new Spread(a(0), a(1) - a(2))
-  val weather = _weather.compose(toInts)
-  val football = (a:Array[String]) => new Spread(a(1),(a(6).toInt - a(8).toInt).abs )
-}
-
 case class WeatherMunging(val lines: List[String]) {
   def dayWithTheSmallestTemperatureSpread : Int =
-    MungerFor(lines,Interpreters.weather).munge
+    MungerFor(lines, a => Weather(a)).munge
 }
 
 case class FootballMunging(val lines: List[String]) {
   def teamWithTheSmallestDifferenceBetweenForAndAgainst:String =
-    MungerFor(lines,Interpreters.football).munge
+    MungerFor(lines, a=> Football(a)).munge
 }
